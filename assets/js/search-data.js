dca84c83986a68e118d4af5979d8dec281892af2
CCamilaC <109802577+CCamilaC@users.noreply.github.com>
2026-05-21 16:37:32 +0000

    Deploying to gh-pages from @ CCamilaC/CCamilaC.github.io@6cc2136259e66a6e5237b16d411e8dabe6516c1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -453,22 +453,22 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-research-assistance-at-verlab",
           title: 'Research Assistance at Verlab',
-          description: "with background image",
+          description: "project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-research-assistance-in-a-drone-project",
+          title: 'Research Assistance in a drone project',
+          description: "project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-avant-competition-team",
+          title: 'AVANT - Competition Team',
+          description: "Competition Team focussed on UAVs",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-aries-competition-team",
+          title: 'ARIES Competition Team',
+          description: "Mars exploratition robotics team",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
