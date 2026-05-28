f7eca191994ccf6bc4d813590ab7bad134ce3182
CCamilaC <109802577+CCamilaC@users.noreply.github.com>
2026-05-28 15:49:13 +0000

    Deploying to gh-pages from @ CCamilaC/CCamilaC.github.io@d16ef908c5e0ff1e57fe26626255b99259c4e08b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Some of the projects I worked on",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -29,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
